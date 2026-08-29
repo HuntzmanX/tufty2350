6fe496cc8d7cd350b5876396bfa1748b94368c0f
HuntzmanX <csmithtuts@gmail.com>
2026-08-29 10:37:50 +0100

    Enable BLE pairing support on Badgeware v2.0.2

--- board/mpconfigboard.h
+++ board/mpconfigboard.h
@@ -1,6 +1,8 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Pimoroni Tufty 2350"
 
+#define MICROPY_PY_BLUETOOTH_ENABLE_PAIRING_BONDING (1)
+
 #define MICROPY_HW_ROMFS_BYTES                  (1 * 1024 * 1024)
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (2 * 1024 * 1024) - MICROPY_HW_ROMFS_BYTES)
 
